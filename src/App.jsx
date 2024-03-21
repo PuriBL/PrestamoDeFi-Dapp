ea1236a9c9e20d7f4ed64ea1cf35e1783ec47aee
Puri <purificacion.bastida@gmail.com>
2024-03-21 13:13:07 +0100

    Actualizar componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { AppLayout } from './components/ui/layouts'
+
 function App() {  
-  return <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+  return (    
+      <AppLayout>
+        <h1 className="text-3xl font-bold underline">
+        Esta es la aplicación descentralizada para PrestamoDeFi creado por Super Puri!</h1>
       
+      </AppLayout>
+  )      
 }
 
 export default App
