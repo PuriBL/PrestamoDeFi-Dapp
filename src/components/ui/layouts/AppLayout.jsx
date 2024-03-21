549a45e14e9e3cc2d5072bc3a7224becd647e472
Puri <purificacion.bastida@gmail.com>
2024-03-21 13:11:46 +0100

    Añadir componente AppLayout

---
+++ src/components/ui/layouts/AppLayout.jsx
@@ -0,0 +1,16 @@
+import {Header, Footer} from '../../index'
+import PropTypes from 'prop-types'
+
+export default function AppLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main className="mx-auto min-h-[85vh]">{children}</main>
+      <Footer />
+    </>
+  )
+  }
+
+  AppLayout.propTypes = {
+    children: PropTypes.node.isRequired
+  }
