795759c99d01c76e5e4b030410ad30fe1b9ab949
Puri <purificacion.bastida@gmail.com>
2024-03-22 00:03:06 +0100

    Añadir boton connectkit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
+import { ConnectKitButton } from 'connectkit';
 export default function Header() {
     return (
-      <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs">
+      <header className="border-b shadow py-2 px-3 flex justify-between items-center">
         {/* Logo para version mobile */}
         <img src="public/png-clipart-recursos-blue-p-letter-illustration-thumbnail.png" width={37} className="sm:hidden" />
         {/* Logo para version desktop */}
         <img src="public/png-clipart-recursos-blue-p-letter-illustration-thumbnail.png" width={200} className="hidden sm:flex" />
-        <button className= "bg-gray-100 rounded-xl px-2 text-sm h-fit py-2">Connect Wallet</button>
+        <ConnectKitButton showBalance/>
       </header>
     )
   }
