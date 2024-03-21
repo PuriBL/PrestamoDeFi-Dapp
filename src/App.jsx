ed41010079c52d4f9333110a1d9931879c2ae8b6
Puri <purificacion.bastida@gmail.com>
2024-03-21 13:49:30 +0100

    Actualizar App añadiendo pagina Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { AppLayout } from './components/ui/layouts'
+import { Home } from './pages'
 
 function App() {  
   return (    
       <AppLayout>
         <h1 className="text-3xl font-bold underline">
         Esta es la aplicación descentralizada para PrestamoDeFi creado por Super Puri!</h1>
-      
+        <Home />
       </AppLayout>
   )      
 }
