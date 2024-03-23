c36b997bf7f318a747e379c91d5c2245c1500601
Puri <purificacion.bastida@gmail.com>
2024-03-23 21:32:37 +0100

    Modificar index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Cliente } from './Cliente';
+export { default as Info } from './Info';
 
