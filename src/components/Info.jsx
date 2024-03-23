8d154b440e5282fd835ea29981d9b206694aecc6
Puri <purificacion.bastida@gmail.com>
2024-03-23 21:32:53 +0100

    Modificar info.jsx

---
+++ src/components/Info.jsx
@@ -0,0 +1,20 @@
+import { Title } from './ui'
+
+export default function Info() {
+    return (
+        <section className="bg-white p-4 shadow w-fit rounded-lg border">
+            <Title>Registro alta de cliente:</Title>
+            <ul className="grid gap-2">
+                <li>Nombre:</li>
+                <li></li>
+            </ul>
+        </section>
+      
+    )
+}
+
+
+
+
+
+
