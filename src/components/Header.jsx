3e2f73aebcba541f339e45fb1dc417bd24f67435
Puri <purificacion.bastida@gmail.com>
2024-03-23 12:36:19 +0100

    Actualizar Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { ConnectKitButton } from 'connectkit';
-export default function Header() {
+  import { ConnectKitButton } from 'connectkit';
+  export default function Header() {
     return (
       <header className="border-b shadow py-2 px-3 flex justify-between items-center">
         {/* Logo para version mobile */}
         <img src="public/png-clipart-recursos-blue-p-letter-illustration-thumbnail.png" width={37} className="sm:hidden" />
         {/* Logo para version desktop */}
         <img src="public/png-clipart-recursos-blue-p-letter-illustration-thumbnail.png" width={200} className="hidden sm:flex" />
-        <ConnectKitButton showBalance/>
+         {/* Botton de conexion wallet con prop showBalance para mostrar balance cuenta */}         
+         <ConnectKitButton showBalance />
       </header>
     )
+
   }
