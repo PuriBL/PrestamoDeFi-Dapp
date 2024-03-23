afedd8b72733e36ec34404c19e8578eaef1c4d35
Puri <purificacion.bastida@gmail.com>
2024-03-23 15:47:25 +0100

    Actualizar

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as Usuario } from './Usuario';
+export { default as Cliente } from './Cliente';
 
