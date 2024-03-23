f56f96e1511b7477c2a93d1741e3a895d2163c5d
Puri <purificacion.bastida@gmail.com>
2024-03-23 21:33:15 +0100

    Modificar ErrorInfo

--- src/components/ui/ErrorInfo.jsx
+++ src/components/ui/ErrorInfo.jsx
@@ -1,10 +1,11 @@
 import { MdInfoOutline }from 'react-icons/md'
 export default function ErrorInfo() { 
-        return (
-            <div className= "flex items-center gap-3 bg-red-200 p-2 w-fit rounded-lg">     
-                <MdInfoOutline className= "text-red-500"  />                           
-                <p className="text xs text-red-500">Error: algo ha salido mal</p>   
-           
+        return (            
+            <div className= "flex items-center gap-3 bg-indigo-500 hover:bg-indigo-700 p-2 px-4 w-fit rounded-lg">
+              <p className="text xs text-white">Reembolsar préstamo</p>   
+             
             </div>
-    )
+        )           
+                
 }
+
