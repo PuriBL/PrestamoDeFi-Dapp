4d37cc71b95c2354067a7e7c707c11f8a6e9c000
Puri <purificacion.bastida@gmail.com>
2024-03-23 21:33:29 +0100

    Modificar TextInput

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,25 +1,37 @@
-import PropTypes from 'prop-types'
-export default function TextInput({type = 'text', onChange, value, placeholder, disabled}) {
-    return (
-        <input
-        type={type} 
-        onChange={onChange}
-        value={value} 
-        placeholder={placeholder} // te permite escribir dentro
-        disabled={disabled}
-        className="border border-gray-300 rounded-lg p-4 outline-none
-        focus: ring-2 focus:ring-indigo-100 focus:border-transparent
-        transition duration-300 ease-in-out disabled:cursor-not-allowed"
-        />
-    )
+//import PropTypes from 'prop-types'
+//export default function TextInput({type = 'text', onChange, value, placeholder, disabled}) {
+    //return (
+        //<input
+        //type={type} 
+        //onChange={onChange}
+        //value={value} 
+        //placeholder={placeholder} // te permite escribir dentro
+        //disabled={disabled}
+        //className="border border-gray-300 rounded-lg p-4 outline-none
+        //focus: ring-2 focus:ring-indigo-100 focus:border-transparent
+        //transition duration-300 ease-in-out disabled:cursor-not-allowed"
+        ///>
+    //)
 
-}
+//}
 
-TextInput.propTypes = {
-    type: PropTypes.string,
-    onChange: PropTypes.func,
-    value: PropTypes.string,
-    placeholder: PropTypes.string,
-    disabled: PropTypes.bool
+//TextInput.propTypes = {
+    //type: PropTypes.string,
+    //onChange: PropTypes.func,
+    //value: PropTypes.string,
+    //placeholder: PropTypes.string,
+    //disabled: PropTypes.bool
 
+//}
+
+
+import { MdInfoOutline }from 'react-icons/md'
+export default function ErrorInfo() { 
+        return (            
+            <div className= "flex items-center gap-3 bg-indigo-500 hover:bg-indigo-700 p-2 px-4 w-fit rounded-lg">
+              <p className="text xs text-white"> Préstamos  de  cliente </p>   
+             
+            </div>
+        )           
+                
 }
