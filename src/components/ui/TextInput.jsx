7e6c1a3667e50996b0fdbb33b5e9002f65513132
Puri <purificacion.bastida@gmail.com>
2024-03-24 15:19:24 +0100

    Actualizar TestInput

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -29,7 +29,7 @@ import { MdInfoOutline }from 'react-icons/md'
 export default function ErrorInfo() { 
         return (            
             <div className= "flex items-center gap-3 bg-indigo-500 hover:bg-indigo-700 p-2 px-4 w-fit rounded-lg">
-              <p className="text xs text-white"> Préstamos  de  cliente </p>   
+              <p className="text xs text-white"> Depositar garantía </p>   
              
             </div>
         )           
