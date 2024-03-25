41fa550ca8e59b1f6e8514365df23d2761c22cb4
Puri <purificacion.bastida@gmail.com>
2024-03-25 22:35:28 +0100

    Actualizar Info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,9 +3,9 @@ import { Title } from './ui'
 export default function Info() {
     return (
         <section className="bg-white p-4 shadow w-fit rounded-lg border">
-            <Title>Registro alta de cliente:</Title>
+            <Title>Saldo Ether:</Title>
             <ul className="grid gap-2">
-                <li>Nombre:</li>
+                <li>0,50 Sepolita ETH</li>
                 <li></li>
             </ul>
         </section>
