fae759c5dc94f432804c1bceb306cca8bdefdc4e
Puri <purificacion.bastida@gmail.com>
2024-03-25 22:36:14 +0100

    Crear fichero InicioAutenti

---
+++ src/pages/Pagina1/InicioAutenticacion.jsx
@@ -0,0 +1,14 @@
+import Home from './Home'
+import { Navbar } from '../../components'
+
+export default function InicioAutenticacion () {
+    return (
+        <>
+        <h1>Esta es la Página 1</h1>
+            <Home />
+            <Navbar />
+        
+        </>
+    )
+
+}
