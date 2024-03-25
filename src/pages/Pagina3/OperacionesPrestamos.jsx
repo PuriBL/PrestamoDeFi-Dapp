6c5c88a1856e9a776eac93f01d4a6b5bd8ce5cbf
Puri <purificacion.bastida@gmail.com>
2024-03-25 22:36:42 +0100

    Crear fichero OperaPrestam

---
+++ src/pages/Pagina3/OperacionesPrestamos.jsx
@@ -0,0 +1,14 @@
+import Home from './Home'
+import {Navbar } from '../../components'
+
+export default function OperacionesPrestamos () {
+    return (
+        <>
+        <h1>Esta es la Página 3</h1>
+            <Home />
+            <Navbar />
+        
+        </>
+    )
+
+}
