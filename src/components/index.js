ed0787163a2e4f0f9aecac6b6d2ad2228e943730
Puri <purificacion.bastida@gmail.com>
2024-03-25 22:35:14 +0100

    Actualizar index components

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Cliente } from './Cliente';
 export { default as Info } from './Info';
-
+export { default as Navbar } from './Navbar';
