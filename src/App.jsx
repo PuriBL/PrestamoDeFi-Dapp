4bf335f4f0d1e568ccf877fb4296c4295e458d1f
Puri <purificacion.bastida@gmail.com>
2024-03-25 22:33:07 +0100

    Actualizar App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { WagmiConfig } from 'wagmi';
 import { AppLayout } from './components/ui/layouts'
 import { config } from './config/wagmi'
 import { Home } from './pages'
+//import { Routes, Route } from 'react-router-dom'
+//import { InicioAutenticacion } from './pages/Pagina1'
+//import { GestionUsuarios } from './pages/Pagina2'
+//import { OperacionesPretamos } from './pages/Pagina3'
+
+// los cambios desde el min. 28 en adelante de la clase 46 , no me valen 
 
 
 function App() {   
    return (   
       <WagmiConfig config={config}>       
         <ConnectKitProvider mode='light'>
-          <AppLayout>
-            <Home />           
+          <AppLayout>            
+            <Home />                    
           </AppLayout>
         </ConnectKitProvider>
       </WagmiConfig>
