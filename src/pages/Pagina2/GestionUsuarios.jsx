3a6bba999961b49264952f530b55396339e32c13
Puri <purificacion.bastida@gmail.com>
2024-03-25 22:36:26 +0100

    Crear fichero GestionUsua

---
+++ src/pages/Pagina2/GestionUsuarios.jsx
@@ -0,0 +1,14 @@
+import Home from './Home'
+import {Navbar } from '../../components'
+
+export default function GestionUsuarios () {
+    return (
+        <>
+        <h1>Esta es la Página 2</h1>
+            <Home />
+            <Navbar />
+        
+        </>
+    )
+
+}
